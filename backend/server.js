e6e3962fc59b6ac828fd87bc5b776e3db94b4f27
jinju2828 <jinju2828@gmail.com>
2023-02-24 11:46:26 -0800

    #8 Add email, password validation

--- backend/server.js
+++ backend/server.js
@@ -16,38 +16,66 @@ app.get("/", (req, res) => {
 
 //try catch
 app.post("/auth/signup", async (req, res) => {
-  const { email, password, password_confirm } = req.body;
-  // validate the data here the email should be an actual email, password should be crypted
-  if (password === password_confirm) {
-      if(password.length > 8) {
-          try {
-              const cryptedPassword = await bcrypt.hash(password, 10); // use await to get the hashed password
-              const userId = uuidv4();
-              const user = {
-                  userId: userId,
-                  userEmail: email,
-                  userPassword: cryptedPassword
-              };
-              console.log({user})
-              //save it in the json file
-              res.status(201).json({
-                  message: "User created successfully!",
-                  user: user
-              });
-          }
-          catch (error) {
-              console.log(`error occurred ${error}`)
-          }
-      } else {
-          res.status(500).json({
-              message: "Password should be 8 characters or more"
-          });
-      }
-  } else { //password doesn't match
-      res.status(500).json({
-          message: "Password doesn't match"
-      });
-  }
+    const { email, password, password_confirm } = req.body;
+    function isEmail(email) {
+        let emailRegex = /\S+@\S+\.\S+/;
+        return emailRegex.test(email);
+    }
+    let isValidEmail = isEmail(email);
+    let upperCaseRegex = /[A-Z]/;
+    let numberRegex = /[0-9]/;
+    let specialCharRegex = /[^a-zA-Z0-9]/;
+    // validate the data here the email should be an actual email, password should be crypted
+    if (isValidEmail && password === password_confirm) {
+        //Check if password length > 8
+        if(password.length >= 8 && password.match(upperCaseRegex) && password.match(numberRegex) && !password.match(specialCharRegex)) {
+            try {
+                const cryptedPassword = await bcrypt.hash(password, 10); // use await to get the hashed password
+                const userId = uuidv4();
+                const user = {
+                    userId: userId,
+                    userEmail: email,
+                    userPassword: cryptedPassword
+                };
+                console.log({user})
+                //save it in the json file
+                res.status(201).json({
+                    message: "User created successfully!",
+                    user: user
+                });
+            }
+            catch (error) {
+                console.log(`error occurred ${error}`)
+            }
+        } else if(password.length < 8 ) {
+            res.status(400).json({
+                message: "Password should be 8 characters or more"
+            });
+        } else if(!password.match(upperCaseRegex)){
+            res.status(400).json({
+                message: "Password should have at least one upper"
+            });
+        } else if(!password.match(numberRegex)){
+            res.status(400).json({
+                message: "Password should have at least one number"
+            });
+        } else if(password.match(specialCharRegex)){
+            res.status(400).json({
+                message: "Password should not have any symbol"
+            });
+        }
+
+
+    }else if(!isValidEmail){
+        res.status(400).json({
+            message: "Wrong email format"
+        });
+    }
+    else { //password doesn't match
+        res.status(400).json({
+            message: "Password doesn't match"
+        });
+    }
 })
 
 
