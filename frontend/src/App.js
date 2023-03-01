8ff46fc43237c6caf7d3a39641bbd17afc91c1ee
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2023-03-01 14:57:54 -0500

    Removee breakpoints package, issue with compatability, and add CSS updates to create modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,48 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import SignupForm from './Components/SignupForm';
+// import { useState, useEffect } from 'react';
+// import SignupForm from './Components/SignupForm';
 import Nav from './Components/Nav';
+import SignInModal from './Components/SignupForm';
 
-// this will need to be conditionally rendered: import LoginForm from './Components/LoginForm'
+import React, { useState } from 'react';
+
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+  
   return (
-    <>
-        <Nav />
-      <div className='welcome-msg-div'>
-        <h1 className='app-title'>Get Started with Cherry On Tech!</h1>
-        <p className='register'>Register with your email</p>
-      </div>
-      <div>
-        <SignupForm />
-      </div>
-    </>
+    <div>
+      <button onClick={toggleModal}>Sign In</button>
+      {showModal && <SignInModal onClose={toggleModal} />}
+    </div>
   );
 };
 
-
-
 export default App;
+
+
+// this will need to be conditionally rendered: import LoginForm from './Components/LoginForm'
+
+// const App = () => {
+//   return (
+//     <>
+//         <Nav />
+//       <div className='welcome-msg-div'>
+//         <h1 className='app-title'>Get Started with Cherry On Tech!</h1>
+//         <p className='register'>Register with your email</p>
+//       </div>
+//       <div>
+//         <SignupForm />
+//       </div>
+//     </>
+//   );
+// };
+
+
+
+// export default App;
 
--- frontend/src/Components/SignUpForm.js
+++ frontend/src/Components/SignUpForm.js
@@ -1,43 +1,87 @@
-import { Box, Button, TextField } from '@mui/material';
-import '../CSS/SignupForm.css'
+import React from 'react';
 
-
-const SignupForm = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // handle form submission logic
-  };
-
-  //button will need to begin as disabled 
-  //see color palate in figma
-
+const SignInModal = ({ onClose }) => {
   return (
-    <Box className='signup-box' sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          label="Email"
-          margin="normal"
-          required
-          fullWidth
-          autoFocus
-        />
-        <TextField
-          label="Password"
-          type="password"
-          margin="normal"
-          required
-          fullWidth
-        />
-        <Button
-          type="submit"
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
-          Sign in
-        </Button>
-      </form>
-    </Box>
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>&times;</span>
+        <h2>Sign Up</h2>
+        <form>
+          <label htmlFor="email">Email </label>
+          <input placeholder="info@cherry.com" type="email" id="email" />
+          <label htmlFor="email"> Repeat Email </label>
+          <input placeholder="info@cherry.com" type="email" id="email" />
+          <label htmlFor="password"> Password </label>
+          <input placeholder="your password" type="password" id="password" />
+          <label htmlFor="password"> Repeat Password </label>
+          <input placeholder="your password" type="password" id="password" />
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
-export default SignupForm;
+export default SignInModal;
+
+
+// import { Box, Button, TextField } from '@mui/material';
+// import '../CSS/SignupForm.css'
+
+
+// const SignupForm = () => {
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     // handle form submission logic
+//   };
+
+//   //button will need to begin as disabled 
+//   //see color palate in figma
+
+//   return (
+//     <div className='form-div'>
+//       <Box className='signup-box' sx={{ width: { xs: '100%', sm: 600 },  display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 4 }}>
+//         <form onSubmit={handleSubmit}>
+//           <TextField
+//             label="Email"
+//             margin="normal"
+//             required
+//             fullWidth
+//             autoFocus
+//           />
+//           <TextField
+//             label="Repeat Email"
+//             margin="normal"
+//             required
+//             fullWidth
+//             autoFocus
+//           />
+//           <TextField
+//             label="Password"
+//             type="password"
+//             margin="normal"
+//             required
+//             fullWidth
+//           />
+//           <TextField
+//             label="Repeat Password"
+//             type="password"
+//             margin="normal"
+//             required
+//             fullWidth
+//           />
+//           <Button
+//             type="submit"
+//             variant="contained"
+//             sx={{ mt: 3, mb: 2}}
+//             style={{backgroundColor: 'green'}}
+//           >
+//             Register
+//           </Button>
+//         </form>
+//       </Box>
+//     </div>
+//   );
+// };
+
+// export default SignupForm;
