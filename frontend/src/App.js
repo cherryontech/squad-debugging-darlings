17bcf852c7bd39e19cda11a90f8eb59a7659f646
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2023-03-01 18:50:28 -0500

    Update modal and add elements needed for style guide

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
-// import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 // import SignupForm from './Components/SignupForm';
 // import Nav from './Components/Nav';
 import SignupModal from './Components/SignupModal'
-// import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-
-import { useState } from 'react';
-
+//Routes need to be set up for link to work
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
@@ -29,24 +27,5 @@ const App = () => {
 export default App;
 
 
-// this will need to be conditionally rendered: import LoginForm from './Components/LoginForm'
 
-// const App = () => {
-//   return (
-//     <>
-//         <Nav />
-//       <div className='welcome-msg-div'>
-//         <h1 className='app-title'>Get Started with Cherry On Tech!</h1>
-//         <p className='register'>Register with your email</p>
-//       </div>
-//       <div>
-//         <SignupForm />
-//       </div>
-//     </>
-//   );
-// };
-
-
-
-// export default App;
 
--- frontend/src/Components/SignupModal.js
+++ frontend/src/Components/SignupModal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import '../CSS/SignupModal.css'
+import { Link } from 'react-router-dom';
 
   const SignupModal = ({ onClose }) => {
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
@@ -17,27 +18,40 @@ import '../CSS/SignupModal.css'
   }
 
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={onClose}>&times;</span>
-        <h2>Sign Up</h2>
-        <form>
-          <label htmlFor="email">Email </label>
-          <input onChange={handleInputChange} placeholder="info@cherry.com" type="email" id="email" />
-          <label htmlFor="email"> Repeat Email </label>
-          <input onChange={handleInputChange} placeholder="info@cherry.com" type="email" id="repeatEmail" />
-          <label htmlFor="password"> Password </label>
-          <input onChange={handleInputChange} placeholder="your password" type="password" id="password" />
-          <label htmlFor="password"> Repeat Password </label>
-          <input onChange={handleInputChange} placeholder="your password" type="password" id="repeatPassword" />
-          <button disabled={isButtonDisabled} className={isButtonDisabled ? "signup-button-disabled" : "signup-button-enabled"} type="submit">Sign Up</button>
-        </form>
-      </div>
+<div className="modal">
+  <div className="modal-content">
+    <span className="close" onClick={onClose}>&times;</span>
+    <div className="modal-header">
+      <p>Sign Up</p>
     </div>
+    <form>
+      <label htmlFor="email">Email </label>
+      <input onChange={handleInputChange} placeholder="info@cherry.com" type="email" id="email" />
+      <label htmlFor="email"> Repeat Email </label>
+      <input onChange={handleInputChange} placeholder="info@cherry.com" type="email" id="repeatEmail" />
+      <label htmlFor="password"> Password </label>
+      <input onChange={handleInputChange} placeholder="your password" type="password" id="password" />
+      <p className="i"><span>&#9432;</span>At least 8 characters and a mix of numbers.</p>
+      <p className="i"><span>&#9432;</span>Must contain Upper & Lower case letters.</p>
+      <p className="i"><span>&#9432;</span>No symbols.</p>
+      <label htmlFor="password"> Repeat Password </label>
+      <input onChange={handleInputChange} placeholder="your password" type="password" id="repeatPassword" />
+      <button disabled={isButtonDisabled} className={isButtonDisabled ? "signup-button-disabled" : "signup-button-enabled"} type="submit">Sign Up</button>
+    </form>
+    <div>
+       <p>Already have an account?</p>
+      <Link to='/login' aria-label="Click to go to log in page">
+        <button className='details-back-btn selector'>Log In</button>
+        </Link>
+    </div>
+  </div>
+</div>
   );
 };
 
 export default SignupModal;
+
+
 
 
   
