ac9fdb89ffb2a16fd9696f0fe514782a13ff7224
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2023-03-01 17:32:34 -0500

    Update index js to wrap app with router for future use

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App'
+import { BrowserRouter } from 'react-router-dom';
+
+
 
 const root = document.getElementById('root');
 render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> 
+      <App />
+    </BrowserRouter>  
   </React.StrictMode>, root);
 
 
-  // <BrowserRouter>
-  //     <App />
-  //   </BrowserRouter>
-
- 
