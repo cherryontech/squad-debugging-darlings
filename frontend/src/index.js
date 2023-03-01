9ee5cf1a2714fd20e87b07ef2e0a14c953fbc0c5
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2023-02-28 19:13:33 -0500

    Update  index file to wrap root in breakpoints npm to correlate with design team breakpoint request

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ReactBreakpoints from 'react-breakpoints'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const breakpoints = {
+  mobile: 320,
+  // mobileLandscape: 480,
+  tablet: 768,
+  // tabletLandscape: 1024,
+  desktop: 1200
+  // desktopLarge: 1500,
+  // desktopWide: 1920,
+}
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <ReactBreakpoints breakpoints={breakpoints}>
+    <App />
+  </ReactBreakpoints>,
+  document.getElementById('root'),
+)
+
+
+
+ 
+
+ 
