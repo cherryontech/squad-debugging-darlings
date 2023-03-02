896410ec61762b00c34865d5400e05d9b4b84250
Susanna Carey <83846677+susannaopal@users.noreply.github.com>
2023-03-02 14:36:19 -0500

    Create post to add new user, and update code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,32 @@ import { useState, useEffect } from 'react';
 import SignupModal from './Components/SignupModal'
 import { Routes, Route } from 'react-router-dom';
 
-//Routes need to be set up for link to work
+//Routes need to be set up for link
+
+// needed for the post
+
+// app.post("/auth/signup", async (req, res) => {
+//   const { email, password, password_confirm } = req.body;
+
+
+
+export const postNewUser = (newUser) => {
+  console.log("newUser", newUser)
+   return fetch('http://localhost:3001/auth/signup', {
+     method: 'POST',
+     body: JSON.stringify({
+       email: newUser.email,
+       password: newItem.password,
+       password_confirm: password_confirm
+     }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+   })
+  .then(response => response.json())
+  .then(error => console.log(error))
+}
 
 const App = () => {
   const [showModal, setShowModal] = useState(true);
--- frontend/src/Components/SignupModal.js
+++ frontend/src/Components/SignupModal.js
@@ -5,7 +5,10 @@ import { Link } from 'react-router-dom';
 
   const SignupModal = ({ onClose }) => {
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false)
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const [isInputValid, setIsInputValid] = useState(false);
   // const [email, setEmail] = useState("")
   // const [repeatEmail, setRepeatEmail] = useState("")
   // const [password, setPassword] = useState("")
@@ -38,12 +41,14 @@ const handleInputChange = (event) => {
   const repeatEmail = form.elements.repeatEmail.value;
   const password = form.elements.password.value;
   const repeatPassword = form.elements.repeatPassword.value;
-
   const isEmailMatch = email === repeatEmail;
   const isPasswordMatch = password === repeatPassword;
   const isPasswordValid = /^[a-zA-Z]{8,}$/.test(password);
+  // const isInputValid = /^\S+@\S+\.\S+$/.test(email);
 
   setIsButtonDisabled(!(isEmailMatch && isPasswordMatch && isPasswordValid));
+  setIsPasswordValid(isPasswordValid);
+  setInputIsValid(isInputValid)
 }
 
 
@@ -61,14 +66,14 @@ const handleInputChange = (event) => {
       <input onChange={handleInputChange} placeholder="info@cherry.com" type="email" id="repeatEmail" />
       <label htmlFor="password"> Password </label>
       <input onChange={handleInputChange} placeholder="your password" type="password" id="password" />
-      <p className="i"><span>&#9432;</span>At least 8 characters and a mix of numbers.</p>
-      <p className="i"><span>&#9432;</span>Must contain Upper & Lower case letters.</p>
-      <p className="i"><span>&#9432;</span>No symbols.</p>
+      <p className={isPasswordValid ? "i" : "i error"}><span>&#9432;</span>At least 8 characters and a mix of numbers.</p>
+      <p className={isPasswordValid ? "i" : "i error"}><span>&#9432;</span>Must contain Upper & Lower case letters.</p>
+      <p className={isPasswordValid ? "i" : "i error"}><span>&#9432;</span>No symbols.</p>
       <label htmlFor="password"> Repeat Password </label>
       <input onChange={handleInputChange} placeholder="your password" type="password" id="repeatPassword" />
       <button disabled={isButtonDisabled} className={isButtonDisabled ? "signup-button-disabled" : "signup-button-enabled"} type="submit">Register Account</button>
     </form>
-    <div>
+    <div className="login-account">
        <p>Already have an account?</p>
       <Link to='/login' aria-label="Click to go to log in page">
         <button className='details-back-btn selector'>Log In</button>
