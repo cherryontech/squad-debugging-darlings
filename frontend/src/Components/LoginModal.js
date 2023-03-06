268f0bef6a8810b96a7697f293ab40204ffaac13
Ana Bennett <anabennett1111@gmail.com>
2023-03-06 10:12:26 -0600

    fixed error on the submit button where there multiple classNames

--- frontend/src/Components/LoginModal.js
+++ frontend/src/Components/LoginModal.js
@@ -1,47 +1,68 @@
-import '../CSS/LoginModal.css';
-import { useState } from 'react';
+import "../CSS/LoginModal.css";
+import { useState } from "react";
 
- export const LoginModal = ({ closeLoginModal }) => {
-   const [email, setEmail] = useState("");
+export const LoginModal = ({ closeLoginModal }) => {
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    // login logic in the next ticket 
+    // login logic in the next ticket
   };
   return (
     <>
       <div className="modal">
         <div className="modal-content-login">
-          <div className='title-login'>
+          <div className="title-login">
             <h1>Welcome back to Cherry on Tech!</h1>
             <p>Log in with your email</p>
-          <div className="reminder-error">
-            <p>Looks like you may already have an account with us. Use your credentials to log in instead.</p>
-          </div>
+            <div className="reminder-error">
+              <p>
+                Looks like you may already have an account with us. Use your
+                credentials to log in instead.
+              </p>
+            </div>
           </div>
           <form className="form" onSubmit={handleSubmit}>
             <div className="form-group-login">
               <label>Email</label>
-              <input type="email" placeholder="info@cherry.com" id="email" name="email" required />
+              <input
+                type="email"
+                placeholder="info@cherry.com"
+                id="email"
+                name="email"
+                required
+              />
             </div>
             <div className="form-group-login">
               <label>Password:</label>
-              <input type="password" placeholder="password" id="password" name="password" required />
+              <input
+                type="password"
+                placeholder="password"
+                id="password"
+                name="password"
+                required
+              />
             </div>
-            <button className="login-button" disabled={isButtonDisabled} className={isButtonDisabled ? "login-button-disabled" : "login-button-enabled"} type="submit">Log In</button>
+            <button
+              className={
+                isButtonDisabled
+                  ? "login-button-disabled"
+                  : "login-button-enabled"
+              }
+              disabled={isButtonDisabled}
+              type="submit"
+            >
+              Log In
+            </button>
           </form>
           <div className="register-account">
             <p>Not a member yet?</p>
-            <button className='register-button'>Register Now</button>
+            <button className="register-button">Register Now</button>
           </div>
         </div>
       </div>
     </>
   );
 };
-
- 
-
