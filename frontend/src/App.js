fc3b5610e6507ac82d2b6afc451592a953fe6340
Ana Bennett <anabennett1111@gmail.com>
2023-03-10 12:13:24 -0600

    get app back to working state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,6 @@ import "./App.css";
 import { useState } from "react";
 import { LoginModal } from "./Components/LoginModal";
 import { SignupModal } from "./Components/SignupModal";
-import { ProgressBarForm } from "./Components/ProgressBar";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
   const [showSignupModal, setSignupShowModal] = useState(true);
@@ -11,26 +9,24 @@ const App = () => {
   const [alertMsg, setAlertMsg] = useState("");
 
   return (
-    <BrowserRouter>
-      <div>
-        {showSignupModal && (
-          <SignupModal
-            open={showSignupModal}
-            openLoginModal={setLoginShowModal}
-            closeSignupModal={setSignupShowModal}
-            setAlertMsg={setAlertMsg}
-          />
-        )}
-        {showLoginModal && !showSignupModal && (
-          <LoginModal
-            open={showLoginModal}
-            closeLoginModal={setLoginShowModal}
-            alertMsg={alertMsg}
-            setAlertMsg={setAlertMsg}
-          />
-        )}
-      </div>
-    </BrowserRouter>
+    <div>
+      {showSignupModal && (
+        <SignupModal
+          open={showSignupModal}
+          openLoginModal={setLoginShowModal}
+          closeSignupModal={setSignupShowModal}
+          setAlertMsg={setAlertMsg}
+        />
+      )}
+      {showLoginModal && !showSignupModal && (
+        <LoginModal
+          open={showLoginModal}
+          closeLoginModal={setLoginShowModal}
+          alertMsg={alertMsg}
+          setAlertMsg={setAlertMsg}
+        />
+      )}
+    </div>
   );
 };
 
