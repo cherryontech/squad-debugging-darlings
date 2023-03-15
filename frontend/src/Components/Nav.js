694307ae7fabed8f7062097751361b77b5f3aa5a
Ana Bennett <anabennett1111@gmail.com>
2023-03-15 12:10:02 -0500

    Routed the user to the sign up page once they press log out

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -1,14 +1,17 @@
 import "../CSS/Nav.css";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = ({ showLogoutButton }) => {
   return (
     <div className="nav">
       <h1 className="placeholder-logo">Logo</h1>
       {showLogoutButton && (
-        <button className="logout-button" onClick={handleLogout}>
-          Logout
-        </button>
+        <Link className="logout-btn" to="/signup">
+          <button className="logout-button" onClick={handleLogout}>
+            Logout
+          </button>
+        </Link>
       )}
     </div>
   );
