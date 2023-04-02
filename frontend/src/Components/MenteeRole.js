7d741cfab27273a26d0039d9bb051a8d95d5010c
ajkarczewski <karczewskiamanda@gmail.com>
2023-04-02 14:05:33 -0500

    removed commented out code

--- frontend/src/Components/MenteeRole.js
+++ frontend/src/Components/MenteeRole.js
@@ -32,68 +32,9 @@ const useStyles = makeStyles({
 
 const MenteeRole = () => {
   const classes = useStyles();
-  // const { token } = useContext(AuthContext);
-  // const decoded = jwt_decode(token);
-  // const [userId, setUserId] = useState(decoded.userId);
   const [role, setRole] = useState("");
   const [isCardSelected, setIsCardSelected] = useState(false);
 
-  // const getUserProfile = async () => {
-  //   try {
-  //     let config = {
-  //       method: "get",
-  //       maxBodyLength: Infinity,
-  //       url: `${api.users.userProfile}/${userId}`,
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //     };
-  //     const response = await axios.request(config);
-
-  //     const { role } = response.data;
-  //     setRole(role);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-  // put the useEffect in here and invoke the fetchuserprofile
-  // useEffect(() => {
-  //   getUserProfile();
-  // }, []);
-
-  // const handleClick = useCallback(
-  //   (role) => () => {
-  //     setRole(role);
-  //     setIsCardSelected(true);
-  //   },
-  //   [setRole]
-  // );
-
-  // const handleContinueClick = async () => {
-  //   // handle continue button click
-  //   try {
-  //     let data = JSON.stringify({
-  //       role,
-  //     });
-  //     let config = {
-  //       method: "patch",
-  //       maxBodyLength: Infinity,
-  //       url: `${api.users.userProfile}/${userId}`,
-  //       headers: {
-  //         Authorization: `Bearer ${token}`,
-  //         "Content-Type": "application/json",
-  //       },
-  //       data: data,
-  //     };
-  //     const response = await axios.request(config);
-  //     console.log(response.data);
-  //     // move to next step of questionnaire
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
 
   return (
     <>
