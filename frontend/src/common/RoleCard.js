0fe252b2d53c5b3d00a9fbea880d3dd2345556a9
Ana Bennett <anabennett1111@gmail.com>
2023-04-03 16:24:34 -0500

    Create Mentor Flow Component

--- frontend/src/common/RoleCard.js
+++ frontend/src/common/RoleCard.js
@@ -5,12 +5,12 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { makeStyles } from "@mui/styles";
 import { CardActionArea } from "@mui/material";
-import mentor from "../assets/images/mentor.png"
+import mentor from "../assets/images/mentor.png";
 import mentee from "../assets/images/mentee.png";
 import pm from "../assets/images/pm.png";
 import developer from "../assets/images/developer.png";
 import designer from "../assets/images/designer.png";
-import { ClassNames } from "@emotion/react";
+// import { ClassNames } from "@emotion/react";
 
 const useStyles = makeStyles({
   root: {
@@ -22,25 +22,33 @@ const useStyles = makeStyles({
     // background: "#F9F9F9",
     // boxshadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
     // borderradius: "16px",
-  }
-})
+  },
+});
 
 export default function ActionAreaCard({ value }) {
   const classes = useStyles();
   return (
     <Card className={classes.root}>
-      <CardActionArea sx={{
-        alignItems: "center",
-        justifyContent: "center"
-      }}>
+      <CardActionArea
+        sx={{
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <CardMedia
           component="img"
           height="140"
-          image={value === 'Mentor' ? mentor :
-                value === 'Product Manager' ? pm :
-                value === 'Designer' ? designer :
-                value === 'Developer' ? developer :
-                mentee}
+          image={
+            value === "Mentor"
+              ? mentor
+              : value === "Project Manager"
+              ? pm
+              : value === "Designer"
+              ? designer
+              : value === "Developer"
+              ? developer
+              : mentee
+          }
           // image={value === 'Mentor' ? mentor : mentee}
           // alt={}
         />
