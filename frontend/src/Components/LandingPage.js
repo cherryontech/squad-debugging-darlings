7113ca37984a560dc000b14e2953d296af394637
Ana Bennett <anabennett1111@gmail.com>
2023-04-12 18:21:45 -0500

    Completed the last section of the landing page

--- frontend/src/Components/LandingPage.js
+++ frontend/src/Components/LandingPage.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import InspireOthers from "../assets/images/InspireOthers.png";
 import Writing from "../assets/images/Writing.png";
 import workHome from "../assets/images/workHome.png";
+import ExampleCard from "../common/ExampleCard";
 
 export default function LandingPage() {
   const navigate = useNavigate();
@@ -50,42 +51,132 @@ export default function LandingPage() {
             <img className="writingImg" src={Writing}></img>
             <div className="bubblesDiv">
               <div className="firstRow">
-                <p className="bubble">#Career Switching</p>
-                <p className="bubble">#Portfolio</p>
-                <p className="bubble">#Interviews</p>
+                <div className="bubble">
+                  <p>#Career Switching</p>
+                </div>
+                <div className="bubble">
+                  <p>#Portfolio</p>
+                </div>
+                <div className="bubble">
+                  <p>#Interviews</p>
+                </div>
               </div>
               <div className="secondRow">
-                <p className="bubble">#General Career Guidance</p>
-                <p className="bubble">#Technical Skills</p>
+                <div className="bubble">
+                  <p>#General Career Guidance</p>
+                </div>
+                <div className="bubble">
+                  <p>#Technical Skills</p>
+                </div>
               </div>
               <div className="thirdRow">
-                <p className="bubble">🧳 Product Manager</p>
-                <p className="bubble">🧳 Designer</p>
-                <p className="bubble">🧳 Developer</p>
+                <div className="bubble">
+                  <p>🧳 Product Manager</p>
+                </div>
+                <div className="bubble">
+                  <p>🧳 Designer</p>
+                </div>
+                <div className="bubble">
+                  <p>🧳 Developer</p>
+                </div>
               </div>
             </div>
           </div>
           <div className="pictureAndBubblesDiv2">
             <div className="bubblesDiv">
               <div className="firstRow">
-                <p className="bubble">#Network Admins</p>
-                <p className="bubble">#Healthcare</p>
-                <p className="bubble">#Ecommerce</p>
+                <div className="bubble">
+                  <p>#Network Admins</p>
+                </div>
+                <div className="bubble">
+                  <p>#Healthcare</p>
+                </div>
+                <div className="bubble">
+                  <p>#Ecommerce</p>
+                </div>
               </div>
               <div className="secondRow">
-                <p className="bubble">#AI</p>
-                <p className="bubble">#Civic tech</p>
-                <p className="bubble">#Education</p>
-                <p className="bubble">#loT</p>
+                <div className="bubble">
+                  <p>#AI</p>
+                </div>
+                <div className="bubble">
+                  <p>#Civic tech</p>
+                </div>
+                <div className="bubble">
+                  <p>#Education</p>
+                </div>
+                <div className="bubble">
+                  <p>#loT</p>
+                </div>
               </div>
               <div className="thirdRow">
-                <p className="bubble">#Financial</p>
-                <p className="bubble">#Web3</p>
-                <p className="bubble">#Cyber Security</p>
-                <p className="bubble">#Cloud</p>
+                <div className="bubble">
+                  <p>#Financial</p>
+                </div>
+                <div className="bubble">
+                  <p>#Web3</p>
+                </div>
+                <div className="bubble">
+                  <p>#Cyber Security</p>
+                </div>
+                <div className="bubble">
+                  <p>#Cloud</p>
+                </div>
               </div>
             </div>
             <img className="workHomeImg" src={workHome}></img>
+          </div>
+        </div>
+        <div className="container-three">
+          <div className="header">
+            <h2>
+              A group of certified mentors with expertise in various areas.
+            </h2>
+            <p>
+              These exceptional mentors are ready and waiting to help you reach
+              your goals.
+            </p>
+          </div>
+          <div className="cardsContainer">
+            <ExampleCard />
+            <ExampleCard />
+            <ExampleCard />
+          </div>
+        </div>
+        <div className="container-four">
+          <div className="header">
+            <h2>FAQ</h2>
+          </div>
+          <div className="explanation">
+            <div className="container">
+              <p className="bold-text">How much does it cost?</p>
+              <p>
+                Good question! We don’t charge any fees from users. With our
+                program, you'll have access to top-notch professional mentors
+                who are eager to help <br></br>you achieve your career goals,
+                all at no cost to you.
+              </p>
+            </div>
+            <div className="container">
+              <p className="bold-text">
+                How do you match mentees with the right mentors?
+              </p>
+              <p>
+                We provide a unique matching survey that helps us effectively
+                evaluate your specific needs to match mentees with the right
+                mentors. By answering a <br></br>few quick questions, we’ll be
+                able to determine your best fit.
+              </p>
+            </div>
+            <div className="container">
+              <p className="bold-text">How are mentors verified?</p>
+              <p>
+                We provide a unique matching survey that helps us effectively
+                evaluate your specific needs to match mentees with the right
+                mentors. By answering a <br></br> few quick questions, we’ll be
+                able to determine your best fit.
+              </p>
+            </div>
           </div>
         </div>
       </div>
