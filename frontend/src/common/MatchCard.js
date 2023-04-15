64bc093c939e92fcb3229ea2e76f0e654970374e
Ana Bennett <anabennett1111@gmail.com>
2023-04-15 14:44:27 -0500

    Try to add the GET request to the MatchCard component

--- frontend/src/common/MatchCard.js
+++ frontend/src/common/MatchCard.js
@@ -1,33 +1,68 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import "../CSS/MatchCard.css";
+// import axios from "axios";
+// import jwt_decode from "jwt-decode";
+// import { AuthContext } from "../Context/AuthContext";
+// import { api } from "../api/api";
+
+function MentorCard() {
+  return (
+    <div className="card">
+      <div className="nameDiv">
+        <p className="bold-text">mentor.name</p>
+        <p>mentor.gender</p>
+        <p>mentor.title</p>
+      </div>
+      <div className="industryDiv">
+        <p className="bold-text">Industry</p>
+        <p>mentor.industries.join(" ")</p>
+      </div>
+      <div className="strengthDiv">
+        <p className="bold-text">Strength</p>
+        <p>mentor.strengths.join(" ")</p>
+      </div>
+      <button className="clickMe" disabled>
+        Book chat
+      </button>
+    </div>
+  );
+}
 
 export default function MatchCard() {
+  // const { token } = useContext(AuthContext);
+  // const decoded = jwt_decode(token);
+  // const [userId, setUserId] = useState(decoded.userId);
+  // const [mentors, setMentors] = useState([]);
+
+  // const getMatches = async () => {
+  //   try {
+  //     let config = {
+  //       method: "get",
+  //       maxBodyLength: Infinity,
+  //       url: `${api.users.userProfile}/${userId}`,
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         "Content-Type": "application/json",
+  //       },
+  //     };
+  //     const response = await axios.request(config);
+
+  //     const { mentors } = response.data;
+  //     setMentors(mentors);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   getMatches();
+  // }, []);
+
   return (
     <>
-      <div className="card">
-        <div className="nameDiv">
-          <p className="bold-text">Isabelle Mcgee</p>
-          <p>She/her</p>
-          <p>🧳 Product Designer</p>
-        </div>
-        <div className="industryDiv">
-          <p className="bold-text">Industry</p>
-          <p>
-            #Healthcare #Entertainment #Ecommerce <br></br>
-            #Cyber Security #Network Admins
-          </p>
-        </div>
-        <div className="strengthDiv">
-          <p className="bold-text">Strength</p>
-          <p>
-            #Career Switching #General Career Guidance <br></br>
-            #Imposter Syndrome
-          </p>
-        </div>
-        <button className="clickMe" disabled>
-          Book chat
-        </button>
-      </div>
+      {/* {mentors.map((mentor) => ( */}
+      <MentorCard />
+      {/* ))} */}
     </>
   );
 }
