cc8b1652d2364ee8bf38a1b11692aceff3cd08b4
Ana Bennett <anabennett1111@gmail.com>
2023-04-17 12:06:05 -0500

    Try to populate the users matched mentors

--- frontend/src/common/MatchCard.js
+++ frontend/src/common/MatchCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, useCallback } from "react";
 import "../CSS/MatchCard.css";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
@@ -6,41 +6,44 @@ import { AuthContext } from "../Context/AuthContext";
 import { api } from "../api/api";
 
 const MentorCard = ({ role, user }) => {
-  console.log({ role, user })
+  console.log({ role, user });
   return (
     <div className="card">
       <div className="nameDiv">
-        <p className="bold-text">{user.firstName} {user.lastName}</p>
+        <p className="bold-text">
+          {user.firstName} {user.lastName}
+        </p>
         <p>{user.pronouns}</p>
         <p>{user.title}</p>
       </div>
       <div className="industryDiv">
         <p className="bold-text">Industry</p>
-        {/* <p> TODO: SHow the industries</p> */}
+        <p> {user.industries}</p>
       </div>
       <div className="strengthDiv">
         <p className="bold-text">Strength</p>
-        {/* <p>{user.strengths.(" ")}</p> */}
+        <p>{user.strengths.join(" ")}</p>
       </div>
-      {
-        role === "Mentee" ?
-          <button className="clickMe" disabled>
-            Book chat
-          </button> : <></>
-      }
-
+      {role === "Mentee" ? (
+        <button className="clickMe" disabled>
+          Book chat
+        </button>
+      ) : (
+        <></>
+      )}
     </div>
   );
-}
+};
 
 export default function MatchCard() {
   // const { token } = useContext(AuthContext);
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI0NWEwMjc4ZS0xMmEzLTQ3YWEtYWRiMy0yNWNjZmQzYWJmY2YiLCJpYXQiOjE2ODE2NjMzNjZ9.gbUJqmMcCuQH5VbkUtqGBHhyceT8PKqZbrvQeknlNxc";
+  const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI0NWEwMjc4ZS0xMmEzLTQ3YWEtYWRiMy0yNWNjZmQzYWJmY2YiLCJpYXQiOjE2ODE2NjMzNjZ9.gbUJqmMcCuQH5VbkUtqGBHhyceT8PKqZbrvQeknlNxc";
   const decoded = jwt_decode(token);
   const [userId, setUserId] = useState(decoded.userId);
   const [matchedUsers, setMatchedUsers] = useState([]);
 
-  const getMatches = async () => {
+  const getMatches = useCallback(async () => {
     try {
       let config = {
         method: "get",
@@ -52,23 +55,32 @@ export default function MatchCard() {
         },
       };
       const response = await axios.request(config);
+      console.log(response);
       const matchingUsers = response.data;
+      console.log(matchingUsers);
       setMatchedUsers(matchingUsers);
     } catch (error) {
       console.error(error);
     }
-  };
+  }, [setMatchedUsers, token]);
 
-  // TODO: Need to useCallBack instead of useEffect or Memo
   useEffect(() => {
     getMatches();
-  }, [setMatchedUsers, matchedUsers]);
-
+  }, [getMatches]);
+  console.log(matchedUsers);
   return (
     <>
-      {matchedUsers.map((matchedUser) => (
-        <MentorCard key={matchedUser.userId} role={matchedUser.role} user={matchedUser} />
-      ))}
+      {matchedUsers.length > 0 ? (
+        matchedUsers.map((matchedUser) => (
+          <MentorCard
+            key={matchedUser.userId}
+            role={matchedUser.role}
+            user={matchedUser}
+          />
+        ))
+      ) : (
+        <p>No matched users found</p>
+      )}
     </>
   );
 }
