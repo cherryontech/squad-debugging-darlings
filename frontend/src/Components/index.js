bb4e766022f87886926d45a4641e03004d1c58ad
YashsviG <girdhar.yashsvi@gmail.com>
2023-04-20 01:14:47 +0530

    minor tweaks

--- frontend/src/Components/index.js
+++ frontend/src/Components/index.js
@@ -1,5 +1,3 @@
-import { formToJSON } from "axios";
-
 export { default as SignupModal } from "./SignupModal";
 export { default as LoginModal } from "./LoginModal";
 export { default as ProgressBarForm } from "./ProgressBarForm";
